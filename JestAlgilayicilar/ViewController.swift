b0236955fa4c58f8b3af1a245170f99791d2aaee
MRSgkce <mursidegokce985@gmail.com>
2024-08-12 11:10:44 +0300

    imageviewüzerine tıklanınca resmin ve label'ın değişmesi

--- JestAlgilayicilar/ViewController.swift
+++ JestAlgilayicilar/ViewController.swift
@@ -9,9 +9,36 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
+    
+    var ankara = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        imageView.isUserInteractionEnabled=true
+        
+        let gestureRecognizer=UITapGestureRecognizer(target:self,action: #selector(gorseldegistir))
+        
+        imageView.addGestureRecognizer(gestureRecognizer)
+        
+        
+        
+        
+      
+    }
+    @objc func gorseldegistir( ){
+
+        if ankara == false {
+            imageView.image = UIImage(named: "ankara")
+            label.text="ankara"
+            ankara=true
+        } else {
+            imageView.image=UIImage(named: "istanbul")
+            label.text="istanbul"
+            ankara=false
+        }
     }
 
 
